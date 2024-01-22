38502ceffef45ea7c929f4d822a9976e3e674fb1
Maksim Kandratsiuk <maximkondratyuk@gmail.com>
2024-01-22 21:00:49 +0300

    feat and fix: add answer 06-build-page and  fix 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,18 +2,17 @@ const fs = require('fs');
 const fsPromises = require('fs/promises');
 const path = require('path');
 
-const fsProm = fsPromises.readdir(path.join(__dirname, 'files'));
-
 function copyDir() {
   fs.mkdir(path.join(__dirname, 'files-copy'), { recursive: true }, (err) => {
     if (err) throw err;
   });
 
-  fsProm.then(item => {
+  fsPromises
+  .readdir(path.join(__dirname, 'files'))
+  .then(item => {
     item.forEach(elem => {
       fsPromises.copyFile(path.join(__dirname, 'files', elem), path.join(__dirname, 'files-copy', elem));
     });
   });
 }
-
 copyDir();
---
+++ 06-build-page/index.js
@@ -0,0 +1,91 @@
+const fs = require('fs');
+const fsPromises = require('fs/promises');
+const path = require('path');
+const assets = path.join(__dirname, 'assets');
+const assetsCopy = path.join(__dirname, 'project-dist/assets');
+
+fs.mkdir(path.join(__dirname, 'project-dist'), { recursive: true }, (err) => {
+  if (err) throw err;
+});
+
+fs.mkdir(path.join(__dirname, 'project-dist/assets'), { recursive: true }, (err) => {
+  if (err) throw err;
+});
+
+
+fs.writeFile(
+  path.join(__dirname, "project-dist", "index.html"),
+  "",
+  (err) => {
+    if (err) throw err;
+  }
+);
+
+fs.writeFile(
+  path.join(__dirname, "project-dist", "style.css"),
+  "",
+  (err) => {
+    if (err) throw err;
+  },
+);
+
+function copyDir(folder, where) {
+  fsPromises
+  .readdir(folder, { withFileTypes: true })
+  .then(item => {
+    item.forEach(file => {
+      if (file.isDirectory()) {
+        copyDir(path.join(folder, file.name), path.join(where, file.name))
+      } else {
+        fs.mkdir(where, { recursive: true }, err => {
+          if (err) throw err;
+        })
+        fsPromises.copyFile(path.join(folder, file.name), path.join(where, file.name))
+      }
+    })
+  })
+}
+copyDir(assets, assetsCopy);
+
+fs.copyFile(path.join(__dirname, 'template.html'), path.join(__dirname, 'project-dist/index.html'), err => {
+  if(err) throw err;
+});
+
+
+(async function addHTML() {
+  await fsPromises.readFile(path.join(__dirname, 'project-dist/index.html'), 'utf-8')
+  .then(async data => {
+    await fsPromises.readdir(path.join(__dirname, 'components'), { withFileTypes: true })
+    .then(async item => {
+      item.forEach(async file => {
+        const ext = path.extname(path.join(__dirname, 'components', file.name))
+        const name = path.basename(path.join(__dirname, 'components',file.name), ext);
+        await fsPromises.readFile(path.join(__dirname, 'components', file.name), 'utf-8')
+        .then(async code => {
+          if (ext === '.html') {
+            await fsPromises.writeFile(path.join(__dirname, "project-dist", "index.html"), data = data.replace(`{{${name}}}`,`\n${code}`));
+          } else {
+            throw new Error('Error type file');
+          }
+        })
+      })
+    })
+  });
+})();
+
+const output = fs.createWriteStream(path.join(__dirname, 'project-dist/style.css'));
+
+const fsProm = fsPromises.readdir(path.join(__dirname, 'styles'));
+fsProm.then(item => {
+  item.forEach(file => {
+    if (path.extname(path.join(__dirname, 'styles', file)) === '.css') {
+      const input = fs.createReadStream(path.join(__dirname, 'styles', file));
+      input.pipe(output);
+    }
+  })
+})
+
+
+
+
+
